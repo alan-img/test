3df68c239b9885a0244ed6c24d9d3656fcd48ebd
qinjiawei <738437340@qq.com>
2024-03-25 15:10:20 +0800

    1、直接向main分支提交

---
+++ src/main/scala/cn/edu/ahut/demo/ScalaDemo.scala
@@ -0,0 +1,16 @@
+package cn.edu.ahut.demo
+
+/**
+ * <p>projectName: test</p>
+ * <p>packageName: cn.edu.ahut.demo</p>
+ * <p>className: JavaDemo</p>
+ * <p>date: 3/25/2024</p>
+ *
+ * @author qinjiawei(alan)
+ * @since JDK8.0
+ * @version 1.0.0
+ */
+
+object ScalaDemo {
+
+}
